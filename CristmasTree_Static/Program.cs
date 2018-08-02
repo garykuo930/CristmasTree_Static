221cca8caceff4d90fbbf3d088537b98f6ffaa12
ji3g4vul3eji <41719417+ji3g4vul3eji@users.noreply.github.com>
2018-08-02 17:41:40 +0800

    Initial commit

---
+++ CristmasTree_Static/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CristmasTree_Static
+{
+    class Program
+    {
+        public static void drawHowManyChars(int HowMany, char Chars)
+        {
+            int height, space, star;
+            for (height = 1; height <= HowMany; height += 1)
+            {
+                for (space = 1; space <= HowMany - height; space += 1)
+                {
+                    Console.Write(" ");
+                }
+                for (star = 1; star <= height * 2 - 1; star += 1)
+                {
+                    Console.Write(Chars);
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main(string[] args)
+        {
+            drawHowManyChars(9, '*');
+        }
+    }
+}
