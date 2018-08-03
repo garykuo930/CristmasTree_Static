569c8f9099c8a3cc7753df7d38e4915acca8a476
ji3g4vul3eji <41719417+ji3g4vul3eji@users.noreply.github.com>
2018-08-03 10:26:58 +0800

    新增方法

--- CristmasTree_Static/Program.cs
+++ CristmasTree_Static/Program.cs
@@ -8,7 +8,7 @@ namespace CristmasTree_Static
 {
     class Program
     {
-        public static void drawHowManyChars(int HowMany, char Chars)
+        public static void drawHowManyChars1(int HowMany, char Chars) //方法1
         {
             int height, space, star;
             for (height = 1; height <= HowMany; height += 1)
@@ -24,9 +24,39 @@ namespace CristmasTree_Static
                 Console.WriteLine();
             }
         }
+        public static void drawHowManyChars2(int HowMany, char Chars) //方法2
+        {
+            for (int i = 1; i <= HowMany; i += 1)
+            {
+                Console.Write(Chars);
+            }
+        }
         static void Main(string[] args)
         {
-            drawHowManyChars(9, '*');
+            //呼叫方法1
+            {
+                Console.WriteLine("Call Method 1\n");
+                Program.drawHowManyChars1(5, '*');
+                Console.WriteLine();
+            }
+            //呼叫方法2
+            { 
+            int height = 5;
+            int space = height - 1;
+            int star = 1;
+
+                Console.WriteLine("Call Method 2\n");
+                for (int i = 1; i <= 5; i++)
+                {
+                    Program.drawHowManyChars2(space, ' ');
+                    Program.drawHowManyChars2(star, '*');
+                    Program.drawHowManyChars2(1, '\n');
+
+                    space -= 1;
+                    star += 2;
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
